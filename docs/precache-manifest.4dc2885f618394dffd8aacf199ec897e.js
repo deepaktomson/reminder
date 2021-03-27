ae8b54b4fe03b8c07329d2dabd13135764a0e22d
Deepak <deepak.tomson@digiapt.com>
2021-03-27 23:47:54 +0530

    reminder app with build

---
+++ docs/precache-manifest.4dc2885f618394dffd8aacf199ec897e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49666c6a2e7dba8649f627dd0e2601db",
+    "url": "/design/index.html"
+  },
+  {
+    "revision": "3259f664fc03f985feac",
+    "url": "/design/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/design/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/design/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3259f664fc03f985feac",
+    "url": "/design/static/js/main.bc7c5f1d.chunk.js"
+  },
+  {
+    "revision": "b93e43e0402f9d80ef05",
+    "url": "/design/static/js/runtime-main.32c48a86.js"
+  }
+]);
